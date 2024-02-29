578fe3779a45d5232ff0f7ead338ede19982771e
sandeepjoshi1 <sjofficial12223@gmail.com>
2024-02-29 16:33:40 +0530

    committed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2024 - 1996;
+
+console.log(calcAge);
