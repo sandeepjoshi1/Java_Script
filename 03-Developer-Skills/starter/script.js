102e19cb33ddccc9acdf7d86f6a148d1a84ae7e9
sandeepjoshi1 <sjofficial12223@gmail.com>
2024-03-03 23:12:30 +0530

    committed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -24,25 +24,25 @@ const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // -find mai value in temp array ?
 // -substract min from max (amplitute) and return it
 
-const calcTempAmplituteNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  // console.log(temps);
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-const amplituteNew = calcTempAmplituteNew(
-  [2, 3, 4, 8, 9],
-  [10, 11, 26, 'error', '2z']
-);
-console.log(amplituteNew);
+// const calcTempAmplituteNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   // console.log(temps);
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplituteNew = calcTempAmplituteNew(
+//   [2, 3, 4, 8, 9],
+//   [10, 11, 26, 'error', '2z']
+// );
+// console.log(amplituteNew);
 
 // PROBLU 2:
 //Function should now receive 2 arrays of temperature
@@ -61,3 +61,42 @@ console.log(amplituteNew);
 
 // console.log(numbers);
 // // results in [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    //c) Fix
+    value: Number(prompt('Degrees celsius')),
+  };
+  //b)Find
+  // console.log(measurement);
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// console.log(measureKelvin());
+
+const calcTempAmplituteNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  // console.log(temps);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplituteNew = calcTempAmplituteNew(
+  [2, 3, 4, 8, 9],
+  [10, 11, 26, 'error', '2z']
+);
+console.log(amplituteNew);
