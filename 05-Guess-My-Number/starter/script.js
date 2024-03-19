d6a6d1bb0787893e5e84a2908a1dc8c2f98a6cd1
sandeepjoshi1 <sjofficial12223@gmail.com>
2024-03-19 08:20:23 +0530

    Use of (.Value)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 16;
+document.querySelector('.score').textContent = 12;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 26;
